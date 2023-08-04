e61a00066b5b0cf24733d65a52b30f7e3f52b48b
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-04 19:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+const promise1 = new Promise((resolve,reject) => {
+	setTimeout (() => {
+		resolve("Promise 1 Fulfilled");
+	},2000)
+})
+
+const promise2 = new Promise((resolve,reject) => {
+	setTimeout (() => {
+		resolve("Promise 2 Fulfilled");
+	},3000)
+})
+
+const promise3 = new Promise((resolve,reject) => {
+	setTimeout (() => {
+		resolve("Promise 3 Fulfilled");
+	},1000)
+})
+
+
+// const promise = [promise1,promise2,promise3];
+
+Promise.all([promise1,promise2,promise3]).then((values) => {
+	const output = document.getElementById("output");
+	output.innerHTML = `<tr>
+          <th>Promise 1</th>
+          <th>${values[0]}</th>
+        </tr>
+		<tr>
+          <th>Promise 2</th>
+          <th>${values[1]}</th>
+        </tr>
+		<tr>
+          <th>Promise 3</th>
+          <th>${values[2]}</th>
+        </tr>`
+})
+
+
+
+
+
+
+
+
+
+
